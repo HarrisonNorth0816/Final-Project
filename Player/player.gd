e0dc04100182afb1dc3ffa0234868a640bd80c4d
HarrisonNorth0816 <hanorth@iu.edu>
2023-11-28 11:38:37 -0500

    gravity

--- Player/player.gd
+++ Player/player.gd
@@ -2,11 +2,17 @@ extends Node2D
 @onready var sprite = $AnimatedSprite2D
 
 var velocity = Vector2.ZERO
-var max_run = 100
+var max_run = 150
 var run_acceleration = 800
+var gravity = 1000
+var max_fall = 160
+
 
 func _process(delta):
 	var direction = sign(Input.get_action_strength("right") - Input.get_action_strength("left"))
+			
+	
+	
 	
 	if direction > 0:
 		sprite.flip_h = false
@@ -19,6 +25,10 @@ func _process(delta):
 		sprite.play("Idle")
 	
 	velocity.x = move_toward(velocity.x, max_run * direction, run_acceleration * delta)
+	velocity.y = move_toward(velocity.y, max_fall, gravity * delta)
 	global_position.x += (velocity.x * delta)
+	global_position.y += (velocity.y * delta)
 	
+	if global_position.y >= 560:
+		global_position.y = 560
 	
