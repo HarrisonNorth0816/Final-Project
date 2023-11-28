b41b155955f4761896357362291b7a3109a9d6f4
Diamondskeleton0 <iwilliams21212122@gmail.com>
2023-11-28 13:49:23 -0500

    basic character movement

--- Player/player.gd
+++ Player/player.gd
@@ -1,34 +1,34 @@
-extends Node2D
-@onready var sprite = $AnimatedSprite2D
+extends CharacterBody2D
 
-var velocity = Vector2.ZERO
-var max_run = 150
-var run_acceleration = 800
-var gravity = 1000
-var max_fall = 160
+const acceleration = 20.0
+const deceleration = 50.0
+const maxWalkSpd = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _process(delta):
-	var direction = sign(Input.get_action_strength("right") - Input.get_action_strength("left"))
-			
-	
-	
-	
-	if direction > 0:
-		sprite.flip_h = false
-	elif direction < 0:
-		sprite.flip_h = true
-	
-	if direction != 0:
-		sprite.play("Run")
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x += direction * acceleration
+		if abs(velocity.x) >= maxWalkSpd:
+			if velocity.x < 0:
+				velocity.x = -maxWalkSpd
+			elif velocity.x > 0:
+				velocity.x = maxWalkSpd
 	else:
-		sprite.play("Idle")
-	
-	velocity.x = move_toward(velocity.x, max_run * direction, run_acceleration * delta)
-	velocity.y = move_toward(velocity.y, max_fall, gravity * delta)
-	global_position.x += (velocity.x * delta)
-	global_position.y += (velocity.y * delta)
-	
-	if global_position.y >= 560:
-		global_position.y = 560
-	
+		velocity.x = move_toward(velocity.x, 0, acceleration)
+
+	move_and_slide()
