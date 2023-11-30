f2df15c2f0e2a63b9fa93d0b2247b8ae2fd36bc7
HarrisonNorth0816 <hanorth@iu.edu>
2023-11-30 10:38:41 -0500

    update

--- Levels/LevelScript6.gd
+++ Levels/LevelScript6.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		if name == "Final":
-			var _target = get_tree().change_scene_to_file("res://Levels/endscreen.tscn")
+		if name == "exit6":
+			var _target = get_tree().change_scene_to_file("res://Levels/level7.tscn")
